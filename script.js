4445e3da233375efc3ab70d7c18b8d6c86d59057
Larisa <blinnikova1@ya.ru>
2022-06-23 23:40:51 +0300

    added functions

--- script.js
+++ script.js
@@ -1,65 +1,76 @@
 'use strict';
 
-let numberOfFilmes;
-
-function start() {
-  numberOfFilmes = +prompt('Сколько фильмов вы уже посмотрели?', '');
-   while(numberOfFilmes == '' || numberOfFilmes == null || isNaN(numberOfFilmes)){
-        numberOfFilmes = +prompt('Сколько фильмов вы уже посмотрели?', '');
-    }
-}
-start();
-
 const personalMovieDV = {
-    count: numberOfFilmes,
-    movies: {},
-    actors: {},
-    genres: [],
-    privat: false
-};
-
-function rememberMyFilms() {
+  count: 0,
+  movies: {},
+  actors: {},
+  genres: [],
+  privat: false,
+  start: function(){
+    personalMovieDV.count = +prompt('Сколько фильмов вы уже посмотрели?', '');
+    while(personalMovieDV.count == '' || personalMovieDV.count == null || isNaN(personalMovieDV.count)){
+      personalMovieDV.count = +prompt('Сколько фильмов вы уже посмотрели?', '');
+    }
+  },
+  rememberMyFilms: function(){
     for(let i = 0; i < 2; i++) {
-        const a = prompt('Один из последних просмотренных фильмов?', ''),
-              b = prompt('На сколько оцените его?', '');
-              if(a != null && b != null && a != '' && b != '' && a.length < 50){
-                personalMovieDV.movies[a] = b;
-                console.log('done!');
-              } else {
-                console.log('error');
-                i--;
-              }      
-    }
-}
-
-//function rememberMyFilms();
-
-function detectPersonalLevel() {
+      const a = prompt('Один из последних просмотренных фильмов?', ''),
+            b = prompt('На сколько оцените его?', '');
+      if(a != null && b != null && a != '' && b != '' && a.length < 50){
+        personalMovieDV.movies[a] = b;
+        console.log('done!');
+        } else {
+        console.log('error');
+        i--;
+        }      
+      }
+    },
+  detectPersonalLevel: function(){
     if(personalMovieDV.count < 10) {
-        console.log('Просмотрено довольно мало фильмов');
-    } else if(personalMovieDV.count >= 10 && personalMovieDV.count < 30){
-        console.log('Вы классический зритель');
-    } else if(personalMovieDV.count >= 30) {
-        console.log('Вы киноман');
-    } else {
-        console.log('Произошла ошибка');
-    }
-}
-
-//function detectPersonalLevel();
-
-function showMyDV(hidden) {
+      console.log('Просмотрено довольно мало фильмов');
+      } else if(personalMovieDV.count >= 10 && personalMovieDV.count < 30){
+      console.log('Вы классический зритель');
+      } else if(personalMovieDV.count >= 30) {
+      console.log('Вы киноман');
+      } else {
+      console.log('Произошла ошибка');
+      }
+    },
+  showMyDV: function(hidden) {
     if (!hidden) {
-        console.log(personalMovieDV);
-    }
-}
-showMyDV(personalMovieDV.privat);
+      console.log(personalMovieDV);
+      }
+    },
+  toggleVisibleMyDV: function(){
+    if(personalMovieDV.privat){
+      personalMovieDV.privat = false;
+      } else {
+      personalMovieDV.privat = true;
+      }
+    },
+  writeYourGenres: function () {
+    for(let i = 1; i <= 2; i++) {
+    //   Другой способ: (i<=3) 
+    // let genre = prompt(`Ваш любимый жанр под номером ${i}`);
+    //   if(genre === '' || genre === null){
+    //     console.log('Вы ввели некорректные данные или не ввели их вовсе');
+    //     i--;
+    //     } else {
+    //     personalMovieDV.genres[i-1] = genre;
+    //     }
+      let genres = prompt(`Введите ваши любимые жанры через запятую`).toLowerCase();
 
-function writeYourGenres() {
- for(let i = 1; i <= 3; i++) {
-    personalMovieDV.genres[i-1] = prompt(`Ваш любимый жанр под номером ${i}`);
-    }
-}
-writeYourGenres();
+      if(genres === '' || genres === null){
+        console.log('Вы ввели некорректные данные или не ввели их вовсе');
+        i--;
+        } else {
+        personalMovieDV.genres = genres.split(',');
+        personalMovieDV.genres.sort();
+        }
+      }
 
-//console.log(personalMovieDV);
+      personalMovieDV.genres.forEach((item, i) => {
+        console.log(`Любимый жанр ${i + 1} - это ${item}`);
+      });
+    }     
+};
